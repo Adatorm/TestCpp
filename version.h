55c979f4c6b89817e326d2ea0fef4005b1decbaa
guner acet <kurama632@gmail.com>
2022-06-28 18:50:36 +0300

    version test

--- version.h
+++ version.h
@@ -1,1 +1,26 @@
-v0.4.49
+
+#ifndef MY_VERSION_SYSTEM_
+#define MY_VERSION_SYSTEM_
+#include <sstream>
+#include <cinttypes>
+
+
+class MyVer
+{
+
+public:
+    std::uint8_t major = 0;
+    std::uint8_t minor = 4;
+    std::uint8_t patch = 49;
+
+    std::string toString() const
+    {
+        std::stringstream ss;
+        ss << major << "." << minor << "." << patch;
+        return ss.str();
+    }
+};
+
+
+
+#endif // MY_VERSION_SYSTEM_
